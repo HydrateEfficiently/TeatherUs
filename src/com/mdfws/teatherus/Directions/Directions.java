38da6cb0f557bc19cdd085424b578b4224caad82
WellHydrated <michaelfry2002@gmail.com>
2014-04-24 20:02:33 +1200

    Build directions from end, with distances

--- src/com/mdfws/teatherus/Directions/Directions.java
+++ src/com/mdfws/teatherus/Directions/Directions.java
@@ -7,6 +7,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.mdfws.teatherus.util.Geom;
+
 public class Directions {
 	
 	private ArrayList<Direction> directions;
@@ -28,10 +31,51 @@ public class Directions {
 	}
 	
 	private void createPoints() {
+		Direction currentDirection = null;
+		Direction nextDirection = null;
+		
 		points = new ArrayList<Point>();
-		points.add(new Point() {{
-			nextDirection = directions.get(0);
-		}});
+		Point currentPoint = null;
+		Point prevPoint = createLastPoint();
+		points.add(prevPoint);
+		
+		for (int i = directions.size() - 1; i >= 0; i--) {
+			currentDirection = directions.get(i);
+			List<LatLng> currentDirectionPoints = currentDirection.getPoints();
+			for (int j = currentDirectionPoints.size() - 1; j >= 0; j--) {
+				currentPoint = createPoint(currentDirectionPoints.get(j), prevPoint, currentDirection);
+				points.add(0, currentPoint);
+				prevPoint = currentPoint;
+			}
+			nextDirection = currentDirection;
+		}
+	}
+	
+	private Point createLastPoint() {
+		return new Point() {{
+			distanceToCurrentDirectionMeters = 0;
+			timeToCurrentDirectionMinutes = 0;
+			distanceToNextDirectionMeters = 0;
+			timeToNextDirectionMinutes = 0;
+			distanceToArrivalMeters = 0;
+			timeToArrivalMinutes = 0;
+			direction = directions.get(directions.size() - 1);
+			nextDirection = null;
+			nextPoint = null;
+		}};
+	}
+	
+	private Point createPoint(LatLng location, final Point next, final Direction currentDirection) {
+		final double distanceToNext = Geom.calculateDistance(location, next.location);
+		final boolean isNewDirection = next.direction != currentDirection;
+		return new Point() {{
+			distanceToCurrentDirectionMeters = isNewDirection ? 0 : nextPoint.distanceToCurrentDirectionMeters + distanceToNext;
+			distanceToNextDirectionMeters = isNewDirection ? 0 : nextPoint.distanceToNextDirectionMeters + distanceToNext;
+			distanceToArrivalMeters = nextPoint.distanceToArrivalMeters + distanceToNext;
+			direction = currentDirection;
+			nextDirection = isNewDirection ? nextPoint.direction : nextPoint.nextDirection;
+			nextPoint = next;
+		}};
 	}
 	
 	public List<Direction> getDirectionsList() {
--- src/com/mdfws/teatherus/directions/Point.java
+++ src/com/mdfws/teatherus/directions/Point.java
@@ -1,10 +1,16 @@
 package com.mdfws.teatherus.directions;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class Point {
-	public int distanceToArrivalMeters;
-	public int timeToArrivalMinutes;
-	public int distanceToNextDirectionMeters;
-	public int timeToNextDirectionMinutes;
+	public LatLng location;
+	public double distanceToCurrentDirectionMeters;
+	public double timeToCurrentDirectionMinutes;
+	public double distanceToNextDirectionMeters;
+	public double timeToNextDirectionMinutes;
+	public double distanceToArrivalMeters;
+	public double timeToArrivalMinutes;
+	public Direction direction;
 	public Direction nextDirection;
 	public Point nextPoint;
 }
