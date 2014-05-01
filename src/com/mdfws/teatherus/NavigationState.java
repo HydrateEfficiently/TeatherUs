d09b1fa993df5b2d909bb5d26309cc49a53df2f0
WellHydrated <michaelfry2002@gmail.com>
2014-05-01 16:35:12 +1200

    Big refactor of navigation state management

--- src/com/mdfws/teatherus/NavigationState.java
+++ src/com/mdfws/teatherus/NavigationState.java
@@ -1,137 +1,148 @@
 package com.mdfws.teatherus;
 
+import java.io.InvalidObjectException;
 import java.util.List;
 
 import com.google.android.gms.maps.model.LatLng;
-import com.mdfws.teatherus.NavigatorEvents.UpdateEventArgs;
-import com.mdfws.teatherus.directions.Direction;
 import com.mdfws.teatherus.directions.Directions;
 import com.mdfws.teatherus.directions.Point;
+import com.mdfws.teatherus.positioning.Position;
 import com.mdfws.teatherus.util.LatLngUtil;
 
 public class NavigationState {
 	
-	private final int MAX_TIME_OFF_ROUTE_MS = 5000;
-	private final int OFF_ROUTE_TOLERANCE_METERS = 10;
-	private final int OFF_ROUTE_TOLERANCE_BEARING = 45;
 	private final int LOOK_AHEAD_POINTS = 5;
 	private final int LOOK_BEHIND_POINTS = 2;
-	private final int MIN_ARRIVAL_DIST_METERS = 10;
 	
-	private NavigatorEvents events;
 	private List<Point> path;
-	private Point destination;
-	private LatLng realLocation;
-	private double realBearing;
-	private LatLng snappedLocation;
-	private double snappedBearing;
-	private double distanceOffRoute;
-	private double bearingOffRoute;
-	private long offRouteStartTime;
-	private Point prevPoint;
-	private int timeToArrival;
-	private int distanceToArrival;
-	private int pathIndex = 0;
-	private boolean hasArrived = false;
-	private boolean isOnRoute = true;
+	private Position position;
+	private LatLng locationOnPath;
+	private double bearingOnPath;
+	private double distanceOffPath;
+	private double bearingDifferenceFromPath;
+	private int currentIndex;
+	private Point currentPoint;
+	private boolean isOnPath;
+	private long offPathStartTime;
+	private boolean isSnapshot;
 	
-	public NavigationState(Directions directions, NavigatorEvents events) {
-		this.events = events;
+	public NavigationState(Directions directions) {
 		path = directions.getPath();
-		destination = path.get(path.size() - 1);
+		currentIndex = 0;
+		currentPoint = null;
+		isOnPath = true;
+		isSnapshot = false;
 	}
 	
-	public void update(LatLng location, double bearing) throws Exception {
-		realLocation = location;
-		realBearing = bearing;
-		if (!hasArrived) {
-			prevPoint = path.get(pathIndex);
-			checkArrival();
-			snapLocationToRoute();
-			snapBearingToRoute();
-			calculateDistanceToArrival();
-			calculateTimeToArrival();
-			checkDirectionChanged();
-			fireUpdate();
-			checkOffRoute();
+	private NavigationState(NavigationState navigationStateSnapshot) {
+		position = navigationStateSnapshot.getPosition();
+		locationOnPath = navigationStateSnapshot.getLocationOnPath();
+		bearingOnPath = navigationStateSnapshot.getBearingOnPath();
+		distanceOffPath = navigationStateSnapshot.getDistanceOffPath();
+		bearingDifferenceFromPath = navigationStateSnapshot.getBearingDifferenceFromPath();
+		currentPoint = navigationStateSnapshot.getCurrentPoint();
+		isOnPath = navigationStateSnapshot.isOnPath();
+		offPathStartTime = navigationStateSnapshot.getOffPathStartTime();
+		isSnapshot = true;
+	}
+	
+	public NavigationState snapshot() {
+		return new NavigationState(this);
+	}
+	
+	public void update(Position position) throws InvalidObjectException {
+		if (isSnapshot) {
+			throw new InvalidObjectException("A NavigationState snapshot cannot be updated");
 		} else {
-			throw new Exception("NavigationState expired, vehicle has arrived at destination.");
+			this.position = position;
+			calculateLocationOnPath();
+			calculateBearingOnPath();
 		}
 	}
 	
-	private void checkArrival() {
-		if (LatLngUtil.distanceInMeters(realLocation, destination.location) <= MIN_ARRIVAL_DIST_METERS) {
-			hasArrived = true;
-			events.OnArrival();
-		}
+	public Point getCurrentPoint() {
+		return currentPoint;
 	}
 	
-	private void snapLocationToRoute() {
-		double closestDistance = Double.MAX_VALUE;
+	public Position getPosition() {
+		return position;
+	}
+	
+	public LatLng getLocation() {
+		return position.location;
+	}
+	
+	public double getBearing() {
+		return position.bearing;
+	}
+	
+	public double getTime() {
+		return position.timestamp;
+	}
+	
+	public LatLng getLocationOnPath() {
+		return locationOnPath;
+	}
+	
+	public double getBearingOnPath() {
+		return bearingOnPath;
+	}
+	
+	public double getDistanceOffPath() {
+		return distanceOffPath;
+	}
+	
+	public double getBearingDifferenceFromPath() {
+		return bearingDifferenceFromPath;
+	}
+	
+	public boolean isOnPath() {
+		return isOnPath;
+	}
+	
+	public void signalOnPath() {
+		isOnPath = true;
+	}
+	
+	public void signalOffPath() {
+		offPathStartTime = position.timestamp;
+		isOnPath = false;
+	}
+	
+	public long getOffPathStartTime() {
+		return offPathStartTime;
+	}
+	
+	private void calculateLocationOnPath() {
+		double bestDistanceOffPath = Double.MAX_VALUE;
 		int bestIndex = 0;
 		LatLng bestLocation = null;
+		Point bestPoint = null;
 		
-		for (int i = Math.max(0, pathIndex - LOOK_BEHIND_POINTS);
-				i <= Math.min(path.size() - 1, pathIndex + LOOK_AHEAD_POINTS);
+		for (int i = Math.max(0, currentIndex - LOOK_BEHIND_POINTS);
+				i <= Math.min(path.size() - 1, currentIndex + LOOK_AHEAD_POINTS);
 				i++) {
 			
 			Point currentPoint = path.get(i);
-			LatLng currentSnappedLoc = LatLngUtil.closestLocationOnLine(currentPoint.location, currentPoint.nextPoint.location, realLocation);
-			double currentDistance = LatLngUtil.distanceInMeters(realLocation, currentSnappedLoc);
-			if (currentDistance < closestDistance) {
-				closestDistance = currentDistance;
+			LatLng currentLocationOnPath = LatLngUtil.closestLocationOnLine(currentPoint.location, currentPoint.nextPoint.location, position.location);
+			double currentDistance = LatLngUtil.distanceInMeters(position.location, currentLocationOnPath);
+			
+			if (currentDistance < bestDistanceOffPath) {
+				bestDistanceOffPath = currentDistance;
 				bestIndex = i;
-				bestLocation = currentSnappedLoc;
+				bestPoint = currentPoint;
+				bestLocation = currentLocationOnPath;
 			}
 		}
 		
-		pathIndex = bestIndex;
-		snappedLocation = bestLocation;
-		distanceOffRoute = closestDistance;
+		distanceOffPath = bestDistanceOffPath;
+		currentIndex = bestIndex;
+		currentPoint = bestPoint;
+		locationOnPath = bestLocation;
 	}
 	
-	private void snapBearingToRoute() {
-		Point currentPoint = path.get(pathIndex);
-		snappedBearing = LatLngUtil.initialBearing(currentPoint.location, currentPoint.nextPoint.location);
-		bearingOffRoute = Math.max(snappedBearing, realBearing) - Math.min(snappedBearing, realBearing);
-	}
-	
-	private void checkOffRoute() {
-		if (distanceOffRoute > OFF_ROUTE_TOLERANCE_METERS || bearingOffRoute > OFF_ROUTE_TOLERANCE_BEARING) {
-			unsnapPosition();
-			if (isOnRoute) {
-				isOnRoute = false;
-				offRouteStartTime = System.currentTimeMillis();
-			} else if (System.currentTimeMillis() - offRouteStartTime > MAX_TIME_OFF_ROUTE_MS) {
-				events.OnVehicleOffRoute();
-			}
-		} else {
-			isOnRoute = true;
-		}
-	}
-	
-	private void unsnapPosition() {
-		snappedLocation = realLocation;
-		snappedBearing = realBearing;
-	}
-	
-	private void calculateDistanceToArrival() {
-		
-	}
-	
-	private void calculateTimeToArrival() {
-		
-	}
-	
-	private void checkDirectionChanged() {
-		Direction currentDirection = path.get(pathIndex).direction;
-		Direction prevDirection = prevPoint.direction;
-		if (currentDirection != prevDirection){
-			events.OnNewDirection(currentDirection);
-		}
-	}
-	
-	private void fireUpdate() {
-		events.OnUpdate(new UpdateEventArgs(snappedLocation, snappedBearing, timeToArrival, distanceToArrival, path.get(pathIndex).direction));
+	private void calculateBearingOnPath() {
+		bearingOnPath = LatLngUtil.initialBearing(currentPoint.location, currentPoint.nextPoint.location);
+		bearingDifferenceFromPath = Math.max(bearingOnPath, position.bearing) - Math.min(bearingOnPath, position.bearing);
 	}
 }
--- src/com/mdfws/teatherus/Navigator.java
+++ src/com/mdfws/teatherus/Navigator.java
@@ -1,30 +1,40 @@
 package com.mdfws.teatherus;
 
+import java.io.InvalidObjectException;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.mdfws.teatherus.directions.Direction;
 import com.mdfws.teatherus.directions.Directions;
+import com.mdfws.teatherus.directions.Point;
 import com.mdfws.teatherus.directions.Route.DirectionsRetrieved;
 import com.mdfws.teatherus.directions.Route;
 import com.mdfws.teatherus.map.Map;
 import com.mdfws.teatherus.positioning.AbstractSimulatedGps;
-import com.mdfws.teatherus.positioning.DebugSimulatedGps;
 import com.mdfws.teatherus.positioning.IGps;
 import com.mdfws.teatherus.positioning.IGps.OnTickHandler;
 import com.mdfws.teatherus.positioning.Position;
+import com.mdfws.teatherus.util.LatLngUtil;
+
 import android.app.Activity;
+import android.util.Log;
 
 public class Navigator {
+	
+	private final int MIN_ARRIVAL_DIST_METERS = 10;
+	private final int OFF_PATH_TOLERANCE_METERS = 10;
+	private final int OFF_PATH_TOLERANCE_BEARING = 45;
+	private final int MAX_TIME_OFF_PATH_MS = 5000;
 	
 	private MapFragment mapFragment;
 	private Map map;
 	private Vehicle vehicle;
 	private VehicleOptions vehicleOptions;
 	private IGps gps;
-	private NavigationState navigationState;
 	private NavigatorEvents events;
-	private Position lastPosition;
-	private long lastTickTime;
+	private Position idlePosition;
+	private NavigationState navigationState;
+	private NavigationState lastNavigationState;
+	private LatLng destination;
 	
 	public Navigator(Activity activity, final IGps gps, VehicleOptions vehicleOptions, NavigatorEvents events) {
 		mapFragment = (MapFragment)activity.getFragmentManager().findFragmentById(R.id.main_map_view);
@@ -34,28 +44,64 @@ public class Navigator {
 		gps.onTick(new OnTickHandler() {
 			@Override
 			public void invoke(Position position) {
-				try {
-					onGpsTick(position);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				onGpsTick(position);
 			}
 		});
 		gps.enableTracking();
 		gps.forceTick();
 	}
 	
-	private void onGpsTick(Position position) throws Exception {
-		lastPosition = position;
-		
+	public void navigateTo(final LatLng location) {
+		Route request = new Route(idlePosition.location, location);
+		request.getDirections(new DirectionsRetrieved() {
+			@Override
+			public void invoke(Directions directions) {
+				destination = location;
+				startNavigation(directions);
+			}
+		});
+	}
+	
+	public boolean isNavigating() {
+		return navigationState != null;
+	}
+	
+	private void startNavigation(Directions directions) {
+		if (!isNavigating()) {
+			navigationState = new NavigationState(directions);
+			prepareMapForNavigation(directions);
+			
+			if (gps instanceof AbstractSimulatedGps) {
+				((AbstractSimulatedGps)gps).followPath(directions.getLatLngPath());
+			}
+		}
+	}
+	
+	private void prepareMapForNavigation(Directions directions) {
+		map.setProjectionMode(Map.ProjectionMode.THREE_DIMENSIONAL);
+		map.lockUi();
+		map.addPolyline(directions.getLatLngPath());
+	}
+	
+	private void onGpsTick(Position position) {
 		if (map == null) {
 			initMap(position);
 		}
 		
 		if (isNavigating()) {
-			navigationState.update(position.location, position.bearing);
+			try {
+				navigationState.update(position);
+			} catch (InvalidObjectException e) {
+				e.printStackTrace();
+				Log.e("Fatal exception in Navigator", e.getMessage());
+			}
+			checkArrival();
+			checkDirectionChanged();
+			checkOffPath();
+			updateVehicleMarker();
+			lastNavigationState = navigationState.snapshot();
 		} else {
-			updateVehicleMarker(position.location, position.bearing);
+			idlePosition = position;
 		}
 	}
 	
@@ -65,65 +111,59 @@ public class Navigator {
 		vehicle = new Vehicle(map, vehicleOptions);
 	}
 	
-	private void updateVehicleMarker(LatLng location, double bearing) {
+	private void checkArrival() {
+		if (LatLngUtil.distanceInMeters(navigationState.getLocation(), destination) <= MIN_ARRIVAL_DIST_METERS) {
+			endNavigation();
+			events.OnArrival();
+		}
+	}
+	
+	private void checkDirectionChanged() {
+		Point currentPoint = navigationState.getCurrentPoint();
+		if (lastNavigationState != null) { 
+			Point lastPoint = lastNavigationState.getCurrentPoint();
+			if (currentPoint != lastPoint) {
+				Direction currentDirection = currentPoint.nextDirection;
+				if (currentDirection != lastPoint.nextDirection) {
+					events.OnNewDirection(currentDirection);
+				}
+			}
+		}
+	}
+	
+	private void checkOffPath() {
+		if (navigationState.getDistanceOffPath() > OFF_PATH_TOLERANCE_METERS ||
+				navigationState.getBearingDifferenceFromPath() > OFF_PATH_TOLERANCE_BEARING) {
+			
+			if (lastNavigationState != null && lastNavigationState.isOnPath()) {
+				navigationState.signalOffPath();
+			} else if (navigationState.getTime() - navigationState.getOffPathStartTime() > MAX_TIME_OFF_PATH_MS) {
+				events.OnVehicleOffRoute();
+			}
+		} else {
+			navigationState.signalOnPath();
+		}
+	}
+	
+	private void updateVehicleMarker() {
+		LatLng location;
+		double bearing;
+		if (navigationState.isOnPath()) {
+			location = navigationState.getLocationOnPath();
+			bearing = navigationState.getBearingOnPath();
+		} else {
+			location = navigationState.getLocation();
+			bearing = navigationState.getBearing();
+		}
+		
 		vehicle.setLocation(location);
 		vehicle.setHeading((float)bearing);
 		map.invalidate();
 	}
 	
-	public void navigateTo(LatLng latLng) {
-		Route request = new Route(lastPosition.location, latLng);
-		request.getDirections(new DirectionsRetrieved() {
-			@Override
-			public void invoke(Directions directions) {
-				startNavigation(directions);
-			}
-		});
-	}
-	
-	private boolean isNavigating() {
-		return navigationState != null;
-	}
-	
-	private void startNavigation(Directions directions) {
-		if (!isNavigating()) {
-			createNavigationState(directions);
-			prepareMapForNavigation(directions);
-			
-			if (gps instanceof AbstractSimulatedGps) {
-				((AbstractSimulatedGps)gps).followPath(directions.getLatLngPath());
-			}
-		}
-	}
-	
-	private void createNavigationState(Directions directions) {
-		navigationState = new NavigationState(directions, new NavigatorEvents() {
-			@Override
-			public void OnVehicleOffRoute() {
-				events.OnVehicleOffRoute();
-			}
-			
-			@Override
-			public void OnNewDirection(Direction direction) {
-				events.OnNewDirection(direction);				
-			}
-			
-			@Override
-			public void OnArrival() {
-				events.OnArrival();
-			}
-
-			@Override
-			public void OnUpdate(UpdateEventArgs args) {
-				updateVehicleMarker(args.locationOnRoute, args.bearingOnRoute);
-				events.OnUpdate(args);
-			}
-		});
-	}
-	
-	private void prepareMapForNavigation(Directions directions) {
-		map.setProjectionMode(Map.ProjectionMode.THREE_DIMENSIONAL);
-		map.lockUi();
-		map.addPolyline(directions.getLatLngPath());
+	private void endNavigation() {
+		destination = null;
+		navigationState = null;
+		lastNavigationState = null;
 	}
 }
